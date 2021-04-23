a0cadd795a4c6477507ec5cb0b911c43d94b4e57
Carlos H. Lemus M <carloslemus@zelline.com>
2021-04-22 20:40:24 -0500

    PEP8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,8 +3,8 @@
 
 
 def canUnlockAll(boxes):
-    """You have n number of locked boxes in front of you. 
-    Each box is numbered sequentially from 0 to n - 1 and 
+    """You have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1 and
     each box may contain keys to the other boxes.
     """
     if not boxes:
