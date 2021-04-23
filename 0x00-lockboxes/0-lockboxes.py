0b154b88b103b42006cfc1f48cc7483035df0a61
Carlos H. Lemus M <carloslemus@zelline.com>
2021-04-22 20:20:17 -0500

    Fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,14 +6,6 @@ def canUnlockAll(boxes):
     """You have n number of locked boxes in front of you. 
     Each box is numbered sequentially from 0 to n - 1 and 
     each box may contain keys to the other boxes.
-
-    * boxes is a list of lists
-    * A key with the same number as a box opens that box
-    * You can assume all keys will be positive integers
-    * There can be keys that do not have boxes
-    * The first box boxes[0] is unlocked
-    * Empty boxes don't have a key to open other boxes
-    * Return True if all boxes can be opened, else return False
     """
     if not boxes:
         return False
